13d48d05300271b716dcd302fda389090429ec36
sangyeop <sangyeop@sirs.kr>
2023-08-29 23:32:41 +0900

    회원가입 페이지 추가 - input component 추가

--- pages/join/index.tsx
+++ pages/join/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import {useGetEmailDuplication} from "@/query/userHooks";
 import styled from "styled-components";
 
@@ -10,7 +10,37 @@ const ContentBox = styled.div`
   width: 100%;
 `
 
+const TitleParagraph = styled.p`
+  font-weight: 700;
+  font-size: 15px;
+`
+
+const InputLabelParagraph = styled.p`
+  font-weight: 700;
+  font-size: 15px;
+`
+
+type InputComponentType = {
+    label: string;
+    value: string;
+    setValue: (value: string) => void
+}
+
+const InputComponent = (props: InputComponentType) => {
+    const {label, value, setValue} = props
+    return (
+        <>
+            <InputLabelParagraph>
+                {label}
+            </InputLabelParagraph>
+            <input type={'text'} value={value} onChange={(e) => setValue(e.target.value)}/>
+        </>
+    )
+}
+
 const JoinComponent: FC = () => {
+
+    const [email, setEmail] = useState<string>('')
 
     const {data} = useGetEmailDuplication('gigic5220@gmail.com',
         {
@@ -24,10 +54,21 @@ const JoinComponent: FC = () => {
         }
     )
 
+    const changeEmail = (value: string) => {
+        setEmail(value)
+    }
+
     return (
         <LayoutBox>
             <ContentBox>
-                회원가입
+                <TitleParagraph>
+                    회원가입
+                </TitleParagraph>
+                <InputComponent
+                    label={'이메일'}
+                    value={email}
+                    setValue={changeEmail}
+                />
             </ContentBox>
         </LayoutBox>
     );
